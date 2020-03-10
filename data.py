6c9e38da8392d35bc979752c4aa4593ec601e5cf
3eeps <55860052+3eeps@users.noreply.github.com>
2020-03-10 16:41:16 -0400

    Add files via upload

--- data.py
+++ data.py
@@ -1,91 +1,74 @@
 # gridrpg-master/data.py
 import pygame, random as rnd
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-
 gameTurns = 256
+
 objectData = {
-    'emptySpace': (10, 0),
-    'exitDoor': (1, 1),
-    'bosses': (1, 2),
-    'enemies': (5, 3),
-    'items': (3, 4)  
-}
-
-objectList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4]
-
-def place_objects():
-    rnd.shuffle(objectList)
-    for square in gridData:
-        for object in objectList:
-            gridData[square][3] = objectList[object]
+'emptySpace': (10, 0),
+'exitDoor': (1, 1),
+'bosses': (1, 2),
+'enemies': (5, 3),
+'items': (3, 4)}
 
 sprTheme0 = {
+'background0': pygame.image.load('assets/images/0/background0.png'),
+'square0': pygame.image.load('assets/images/0/square0.png'),
+'empty0': pygame.image.load('assets/images/0/square0n.png'),
+'exit0': pygame.image.load('assets/images/0/square0e.png'),
+'item0': pygame.image.load('assets/images/0/square0i.png'),
+'enemy0': pygame.image.load('assets/images/0/square0m.png'),
+'boss0': pygame.image.load('assets/images/0/square0b.png')}
 
-    'background0': pygame.image.load('assets/images/0/background0.png'),
-    'square0': pygame.image.load('assets/images/0/square0.png'),
-    'empty0': pygame.image.load('assets/images/0/square0n.png'),
-    'exit0': pygame.image.load('assets/images/0/square0e.png'),
-    'item0': pygame.image.load('assets/images/0/square0i.png'),
-    'enemy0': pygame.image.load('assets/images/0/square0m.png'),
-    'boss0': pygame.image.load('assets/images/0/square0b.png')
-}
-
-spriteList0 = [sprTheme0['empty0'], sprTheme0['exit0'], sprTheme0['item0'], sprTheme0['enemy0'], sprTheme0['boss0']]
+objectList = [0, 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 0, 0, 0, 0, 0]
 
 gridCoords = {
-# column 1
 11: (160, 125),
 12: (160, 189),
 13: (160, 253),
 14: (160, 317),
-# column 2
+
 21: (224, 125),
 22: (224, 189),
 23: (224, 253),
 24: (224, 317),
-# column 3
+
 31: (288, 125), 
 32: (288, 189), 
 33: (288, 253), 
 34: (288, 317),
-# column 4
+
 41: (352, 125),
 42: (352, 189),
 43: (352, 253),
 44: (352, 317),
-# column 5    
+   
 51: (416, 125), 
 52: (416, 189),
 53: (416, 253),
-54: (416, 317)
-}
+54: (416, 317)}
 
 gridData = {
-# column 1
 11: [(160, 224), (125, 189), 0, 0],
 12: [(160, 224), (189, 253), 0, 0],
 13: [(160, 224), (253, 317), 0, 0],
 14: [(160, 224), (317, 381), 0, 0],
-# column 2
+
 21: [(224, 288), (125, 189), 0, 0],
 22: [(224, 288), (189, 253), 0, 0],
 23: [(224, 288), (253, 317), 0, 0],
 24: [(224, 288), (317, 381), 0, 0],
-# column 3 
+
 31: [(288, 352), (125, 189), 0, 0],
 32: [(288, 352), (189, 253), 0, 0],
 33: [(288, 352), (253, 317), 0, 0],
 34: [(288, 352), (317, 381), 0, 0],
-# column 4 
+
 41: [(352, 416), (125, 189), 0, 0],
 42: [(352, 416), (189, 253), 0, 0],
 43: [(352, 416), (253, 317), 0, 0],
 44: [(352, 416), (317, 381), 0, 0],
-# column 5 
+ 
 51: [(416, 480), (125, 189), 0, 0],
 52: [(416, 480), (189, 253), 0, 0],
 53: [(416, 480), (253, 317), 0, 0],
-54: [(416, 480), (317, 381), 0, 0]
-}
+54: [(416, 480), (317, 381), 0, 0]}
--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import pygame, data, random as rnd
 
 from pygame.locals import *
 pygame.init(); gameFramerate = pygame.time.Clock()
-gameWindow = pygame.display.set_mode((data.SCREEN_WIDTH, data.SCREEN_HEIGHT))
+gameWindow = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('Gridwerks -dev')
 
 sprTheme0 = data.sprTheme0
 gridData = data.gridData
 gridCoords = data.gridCoords
-spriteList0 = data.spriteList0
+objectList = data.objectList
 
+def add_objects():
+    rnd.shuffle(objectList)
+    for grid_key, object_value in zip(gridData, objectList):
+        gridData[grid_key][3] = object_value
+
+add_objects()
+    
 gameWindow.blit(sprTheme0['background0'].convert(), (0, 0))
-for spritePos in gridCoords:
-    gameWindow.blit(sprTheme0['square0'].convert(), gridCoords[spritePos])
+for spr in gridCoords:
+    gameWindow.blit(sprTheme0['square0'].convert(), gridCoords[spr])
 
 gameRunning = True
 while gameRunning == True:
@@ -33,6 +40,15 @@ while gameRunning == True:
             for loc in gridData:
                 if mx in range(gridData[loc][0][0], gridData[loc][0][1]) and my in range(gridData[loc][1][0], gridData[loc][1][1]) and gridData[loc][2] == 0:
                     gridData[loc][2] = 1
-                    gameWindow.blit(spriteList0[rnd.randrange(0, 3)].convert(), gridCoords[loc])
-
+                    if gridData[loc][3] == 0:
+                        gameWindow.blit(sprTheme0['empty0'].convert(), gridCoords[loc])
+                    if gridData[loc][3] == 1:
+                        gameWindow.blit(sprTheme0['exit0'].convert(), gridCoords[loc])
+                    if gridData[loc][3] == 2:
+                        gameWindow.blit(sprTheme0['item0'].convert(), gridCoords[loc])
+                    if gridData[loc][3] == 3:
+                        gameWindow.blit(sprTheme0['enemy0'].convert(), gridCoords[loc])
+                    if gridData[loc][3] == 4:
+                        gameWindow.blit(sprTheme0['boss0'].convert(), gridCoords[loc])
+                    
     pygame.display.flip()
