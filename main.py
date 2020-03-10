8ea373b38a79585b221439082f8cac40233b94ec
3eeps <55860052+3eeps@users.noreply.github.com>
2020-03-10 14:48:14 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 # grid-master/main.py
 import pygame, data, random as rnd
 
-# build game environment
 from pygame.locals import *
 pygame.init(); gameFramerate = pygame.time.Clock()
 gameWindow = pygame.display.set_mode((data.SCREEN_WIDTH, data.SCREEN_HEIGHT))
-pygame.display.set_caption('GridRPG -dev')
+pygame.display.set_caption('Gridwerks -dev')
 
-sprites = data.sprites
-ranges = data.ranges
-coords = data.coords
-gameWindow.blit(sprites['background0'].convert(), (0, 0))
-for sprite_pos in data.coords:
-    gameWindow.blit(sprites['square0'].convert(), coords[sprite_pos])
+sprTheme0 = data.sprTheme0
+gridData = data.gridData
+gridCoords = data.gridCoords
+spriteList0 = data.spriteList0
+
+gameWindow.blit(sprTheme0['background0'].convert(), (0, 0))
+for spritePos in gridCoords:
+    gameWindow.blit(sprTheme0['square0'].convert(), gridCoords[spritePos])
 
 gameRunning = True
 while gameRunning == True:
@@ -29,9 +30,9 @@ while gameRunning == True:
         elif event.type == MOUSEBUTTONDOWN:
             mx, my = pygame.mouse.get_pos()
             
-            for col in ranges:
-                if mx in range(ranges[col][0][0], ranges[col][0][1]) and my in range(ranges[col][1][0], ranges[col][1][1]) and ranges[col][2] == 0:
-                    ranges[col][2] = 1
-                    gameWindow.blit(data.spriteList[rnd.randrange(0, 3)].convert(), coords[col])
+            for loc in gridData:
+                if mx in range(gridData[loc][0][0], gridData[loc][0][1]) and my in range(gridData[loc][1][0], gridData[loc][1][1]) and gridData[loc][2] == 0:
+                    gridData[loc][2] = 1
+                    gameWindow.blit(spriteList0[rnd.randrange(0, 3)].convert(), gridCoords[loc])
 
     pygame.display.flip()
